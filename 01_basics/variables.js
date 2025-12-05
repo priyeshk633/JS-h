2bcb581f68876cc4d5ed1afa67690d79b208ab26
priyeshk633 <aec.cse2024priyeshkumar@gmail.com>
2025-12-05 23:09:54 +0000

    dev

---
+++ 01_basics/variables.js
@@ -0,0 +1,10 @@
+const accountId = 1445
+
+let accountEmail="abc@gmail.com"
+var accountPass="123"
+accountCity="jaipur "
+//no use of var
+
+
+console.log(accountId)
+console.table([accountEmail, accountPass])
