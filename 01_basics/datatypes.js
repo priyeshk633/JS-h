ff09db56646046762f8d4c54485a4aa511df90db
priyeshk633 <aec.cse2024priyeshkumar@gmail.com>
2025-12-05 23:09:23 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,5 @@
+"use strict"; //newer version
+// alert(3+3) using nodejs,not browser
+//console.log(3+3)
+//documentation ECMA ,tc39.es,MDN
+//number, bigint, string "", boolean, null(type object), undefined(type), symbols, objects
