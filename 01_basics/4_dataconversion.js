7ef3105959f477d9c73a36cbb29b26cb85225b0c
priyeshk633 <aec.cse2024priyeshkumar@gmail.com>
2025-12-06 21:09:55 +0000

    str to num

--- 01_basics/4_dataconversion.js
+++ 01_basics/4_dataconversion.js
@@ -12,3 +12,21 @@ console.log(typeof valueInNumber)//Nan
 //"" => false
 //"abc" => true
 //33 into string= 33, type string
+let value = 3
+let negValue = -value
+ console.log(negValue)
+
+ console.log(2**3) //2 ka power 3
+
+let str1 = "hello"
+let str2 = " pk"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1"+2+2) //122
+console.log(1+2+"2") //32
+console.log(+true)
+console.log(+"")
+
+num1 = num2 =num3 = 2+2
