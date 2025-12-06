d6f4be1f8732fe52736f52b315719c4b74260848
priyeshk633 <aec.cse2024priyeshkumar@gmail.com>
2025-12-06 14:36:47 +0000

    conversion

---
+++ 01_basics/4_dataconversion.js
@@ -0,0 +1,14 @@
+let score = "33"
+console.log(typeof (score))
+
+console.log(typeof score)
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)//Nan
+
+//conversion "33" => 33
+//"33abc" => Nan
+//true =>1
+//1 => true
+//"" => false
+//"abc" => true
+//33 into string= 33, type string
