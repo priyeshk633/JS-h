689b4c516302cd5d2af2fa7c60e79e725f659d80
priyeshk633 <aec.cse2024priyeshkumar@gmail.com>
2025-12-07 13:07:38 +0000

    math random

---
+++ 01_basics/math.js
@@ -0,0 +1,24 @@
+const score = 400
+console.log(score)
+
+/*const balance = new Number(100)
+console.log(balance)
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(1));
+
+const otherNumber = 123.876; //1123.876
+console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN'));
+*/
+//MATHS
+console.log(Math.abs(-4)) //floor,ceil,round,min,max
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+
+const min = 10;
+const max = 20;
+console.log(Math.floor(Math.random()* (max-min+1))+ min);
