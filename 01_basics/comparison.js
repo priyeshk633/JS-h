bf942a4f6e337fcd9554d5b84180880443ab72a1
priyeshk633 <aec.cse2024priyeshkumar@gmail.com>
2025-12-07 10:43:21 +0000

    stack and heap

--- 01_basics/comparison.js
+++ 01_basics/comparison.js
@@ -25,3 +25,21 @@ let myObj={
     age: 20 ,
 }
 //type-object
+
+//stack(primitive)  heap(non-primitive)
+let myname = "Pk" 
+let anothername = myname
+anothername = "ak"
+console.log(myname)
+console.log(anothername)
+//access to a copy(stack),original refer(heap)
+
+let userOne = {
+    email: "ak@gmail.com",
+    upi: "ak@ybl",
+}
+let userTwo = userOne
+
+userTwo.email = "pk@gmail.com"
+console.log(userOne.email)
+console.log(userTwo.email)
